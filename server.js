7c8050d48b8efbce1ba7c63fd3ef25c486521e26
jankaitravel-code <jank.ai.travel@gmail.com>
2025-12-15 15:53:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
 
 // Webhook verification (Meta will call this)
 app.get("/webhook", (req, res) => {
-  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+  const VERIFY_TOKEN = "Jank_ai";
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
