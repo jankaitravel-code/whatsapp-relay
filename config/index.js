362cf63ef5d15eae259cc3c980ccb52fdb45270b
jankaitravel-code <jank.ai.travel@gmail.com>
2025-12-17 14:44:35 +0530

    Update index.js
    Require WHATSAPP_TOKEN from environment

--- config/index.js
+++ config/index.js
@@ -6,9 +6,7 @@
 const config = {
   whatsapp: {
     verifyToken: process.env.VERIFY_TOKEN,
-    accessToken:
-      process.env.WHATSAPP_TOKEN ||
-      "EAFoqwCGEN2oBQFnIXZCtXh3t8sGX2pulg0YZAlcJfB31GvJU3Rp8ynlct3yNCgZCHQfuDwJoQ16pKwvPcKFV5i8CYWrVLwpoX8dvKGvmhXCwaxNlS4wc0w34jCwooZAUo4xe6AdyobVWmoU36MdtBmIRZAAoPFFuHAtnasF4SjSIKSxRmeYxbz6bGQcRJBgOEZA5Hl9JZBdGq5dorvIHbhKEpa5ZCxDICB16eIDRZA3pNxBREPZAZCxx3OSXldzar3ZBg7sUCrlBhqW2suzTA4LDgy3sRV0VLS6ZCHyabey9l",
+    accessToken: process.env.WHATSAPP_TOKEN,
     phoneNumberId: process.env.PHONE_NUMBER_ID
   },
 
