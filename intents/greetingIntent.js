9db5ffaa100f4ed6a19612afe3108cc0fd1f9839
jankaitravel-code <jank.ai.travel@gmail.com>
2025-12-18 14:37:50 +0530

    Create greetingIntent.js

---
+++ intents/greetingIntent.js
@@ -0,0 +1,17 @@
+/**
+ * Greeting Intent (stub)
+ * Will be implemented in 7.2.3
+ */
+
+function canHandle(text) {
+  return false;
+}
+
+async function handle(context) {
+  // No-op for now
+}
+
+module.exports = {
+  canHandle,
+  handle
+};
