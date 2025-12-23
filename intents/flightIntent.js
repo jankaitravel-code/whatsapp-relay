0758acf290e599527ffd12188676360227aed169
jankaitravel-code <jank.ai.travel@gmail.com>
2025-12-23 17:57:07 +0530

    Update flightIntent.js

--- intents/flightIntent.js
+++ intents/flightIntent.js
@@ -245,7 +245,8 @@ async function handle(context) {
   if (
     conversation?.state === "COLLECTING" &&
     conversation.flightQuery &&
-    !conversation.flightQuery.date
+    !conversation.flightQuery.date &&
+  + !conversation.flightQuery.returnDate
   ) {
     const dateMatch = rawText.match(/^\d{4}-\d{2}-\d{2}$/);
 
