90e928ee13016149579bbe9b049634a0485c232c
jankaitravel-code <jank.ai.travel@gmail.com>
2025-12-24 14:07:27 +0530

    Update fallbackIntent.js

--- intents/fallbackIntent.js
+++ intents/fallbackIntent.js
@@ -7,7 +7,7 @@ async function handle({ from, sendWhatsAppMessage }) {
   await sendWhatsAppMessage(
     from,
     "I can help with flights ✈️\n\n" +
-    "Try:\nflight from bengaluru to mumbai on 2025-12-25"
+    "Try saying:\nflights"
   );
 }
 
