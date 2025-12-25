6ffc715a4066287143b8d380c7f45829614e1a75
jankaitravel-code <jank.ai.travel@gmail.com>
2025-12-25 11:05:37 +0530

    Update greetingIntent.js

--- intents/greetingIntent.js
+++ intents/greetingIntent.js
@@ -19,7 +19,7 @@ async function handle({ from, sendWhatsAppMessage }) {
   await sendWhatsAppMessage(
     from,
     "Hi 👋 I’m Jank.ai, your travel assistant.\n\n" +
-    "Currently, I can help you find flights. Try saying:\n" +
+    "Currently, I can help you find flights. Try replying:\n" +
     "Flights"
   );
 }
