8775147ded522b3907e3154ad1e65ddfdd455bf9
jankaitravel-code <jank.ai.travel@gmail.com>
2026-01-09 17:08:24 +0530

    Update attachFlexibilityRisk.js

--- services/flexibility/attachFlexibilityRisk.js
+++ services/flexibility/attachFlexibilityRisk.js
@@ -1,3 +1,5 @@
+const { log } = require("../../utils/logger");
+
 const { normalizeFareRules } = require("../fareRules/normalizeFareRules");
 const { scoreFareFlexibility } = require("./scoreFareFlexibility");
 
@@ -5,9 +7,25 @@ function attachFlexibilityRisk(flightOffer) {
   if (!flightOffer) return flightOffer;
 
   const rules = normalizeFareRules(flightOffer);
-  if (!rules) return flightOffer;
+  if (!rules) {
+    log("FLEX_RISK_MISSING", {
+      flightId: flightOffer.id,
+      airline: flightOffer.airlineCode,
+      fareFamily: flightOffer.fareFamily,
+      hasFareRules: false
+    });
+
+    return flightOffer;
+  }
 
   const risk = scoreFareFlexibility({ fareRules: rules });
+
+  log("FLEX_RISK_ATTACHED", {
+    flightId: flightOffer.id,
+    airline: flightOffer.airlineCode,
+    fareFamily: flightOffer.fareFamily,
+    hasFareRules: true
+  });
 
   return {
     ...flightOffer,
